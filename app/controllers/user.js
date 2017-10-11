c0e90cad94630c0388ae7314f3dbc766960d299b
Jai Rana <jkrana008@gmail.com>
2017-10-11 16:12:22 +0530

    User Login

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -25,20 +25,23 @@ function addUser(req, res) {
 
 function login(req, res) {
     User.findOne({
-        name: req.body.name
+        email: req.body.email
     }, function (err, user) {
         if (err) throw err;
 
         if (!user) {
-            res.json({ success: false, message: 'User not found' });
+            res.status(400);
+            return res.json({ success: false, message: 'User not found' });
         } else if (user) {
             if (!bcrypt.compareSync(req.body.password, user.password)) {
-                res.json({ success: false, message: 'Wrong password' });
+                res.status(400);
+                return res.json({ success: false, message: 'Wrong password' });
             } else {
                 console.log(user);
-                var token = jwt.sign({ data: user }, config.secret, { expiresIn: '1h' });
+                var token = jwt.sign({ data: user }, config.secret, { expiresIn: '30 days' });
                 
-                res.json({
+                res.status(200);
+                return res.json({
                     success: true,
                     message: 'Enjoy your token',
                     token: token,
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var UserController = require('../controllers/user');
+var authMiddleware = require('../middlewares/auth.middleware');
+
 
 router.post('/signup', UserController.addUser);
 router.post('/login', UserController.login);
-router.post('/fetch', UserController.getUsers);
+router.post('/fetch',authMiddleware, UserController.getUsers);
 
 router.get('/', function(req, res, next){
     res.status(200);
